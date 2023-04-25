6ccf13f51858a988312b3ec41342821932b453ef
J. Rafael <jose.rafael@usp.br>
2023-04-25 09:56:12 -0300

    start logging implementation

--- app/run_etl_cmb.py
+++ app/run_etl_cmb.py
@@ -1,26 +1,31 @@
 import os
 import sys
 import datetime
+import logging
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(APP_DIR))
 
 from tools.telegram import Telegram
 from importlib import import_module
 
 
-telegram = Telegram()
+telegram = Telegram()   
 
-def log_and_telegram(message):
-    print(message)
-    telegram.sendMessage(message)
-    
+
+def configure_logs():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s;%(name)s;%(levelname)s;%(message)s',
+        filename=f'{APP_DIR}/data_temp/logs/run_etl_cmb.log',
+        filemode='a'
+    )
 
 
 def execute_scripts():
     
     # Loop through the files and run the 'run' method on each one
-    scripts_folder = f'{SCRIPT_DIR}/scripts'
+    scripts_folder = f'{APP_DIR}/scripts'
     sys.path.append(scripts_folder)
     
     for file in os.listdir(scripts_folder):
@@ -35,14 +40,20 @@ def execute(file):
     
     try:
         module.run()
-        log_and_telegram(f'Finished successfully: {file}')
+        telegram.sendMessage(f'Finished successfully: {file}')
     except Exception as e:
-        log_and_telegram(f'Error running file: {file} --> {e}')
+        telegram.sendMessage(f'Error running file: {file} --> {e}')
+
 
 
 
 
 if __name__ == '__main__':
-    log_and_telegram(f"Starting ETL at: {datetime.datetime.now().strftime('%H:%M:%S')}")
+    
+    configure_logs()
+    
+    telegram.sendMessage(f"Starting ETL at: {datetime.datetime.now().strftime('%H:%M:%S')}")
+    
     execute_scripts()
-    log_and_telegram(f"Finished ETL at: {datetime.datetime.now().strftime('%H:%M:%S')}")
+    
+    telegram.sendMessage(f"Finished ETL at: {datetime.datetime.now().strftime('%H:%M:%S')}")
