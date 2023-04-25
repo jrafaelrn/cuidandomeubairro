6870a2ff7f3b83eb9a47cc78202c92cc570e74ab
J. Rafael <jose.rafael@usp.br>
2023-04-24 21:25:23 -0300

    finished start method 'run' each script file

--- app/run_etl_cmb.py
+++ app/run_etl_cmb.py
@@ -5,28 +5,44 @@ import datetime
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from dotenv import load_dotenv
 from tools.telegram import Telegram
+from importlib import import_module
 
 
 telegram = Telegram()
+
 
 def log_and_telegram(message):
     global telegram
     print(message)
     telegram.sendMessage(message)
+    
+
+
+def execute_scripts():
+    
+    # Loop through the files and run the 'run' method on each one
+    scripts_folder = f'{SCRIPT_DIR}/scripts'
+    sys.path.append(scripts_folder)
+    
+    for file in os.listdir(scripts_folder):
+        if file.endswith('.py'):
+            module = import_module(file.replace('.py', ''))
+            execute(file, module)
+            
+            
+def execute(file, module):
+    print(f'Running file: {file}')
+    try:
+        module.run()
+        log_and_telegram(f'Finished successfully: {file}')
+    except Exception as e:
+        log_and_telegram(f'Error running file: {file} --> {e}')
+
 
 
 
 if __name__ == '__main__':
-    
-    load_dotenv()
-    
     log_and_telegram(f"Starting ETL at: {datetime.datetime.now().strftime('%H:%M:%S')}")
-
-    # Loop through the files and run the 'run' method on each one
-    scripts_folder = f'{SCRIPT_DIR}/scripts'
-    for file in os.listdir(scripts_folder):
-        if file.endswith('.py'):
-            print(f'Running file: {file}')
-            os.system(f'python {scripts_folder}/{file}')
+    execute_scripts()
+    log_and_telegram(f"Finished ETL at: {datetime.datetime.now().strftime('%H:%M:%S')}")
